d99167556f0852d4203580893278fd056022a67b
Niv Sardi <xaiki@evilgiggle.com>
2023-01-09 20:51:17 +0000

    map_utils: always select high resolution tiles
    we actually never properly exported devicePixelRatio... so...
    
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/map_utils.ts
+++ src/map_utils.ts
@@ -1,9 +1,9 @@
 export const URLs = {
-    "CartoDB Voyager": (x, y, z) => `https://${"abc"[Math.abs(x + y) % 3]}.basemaps.cartocdn.com/rastertiles/voyager/${z}/${x}/${y}${devicePixelRatio > 1 ? "@2x" : ""}.png`,
-    "Stamen Terrain": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/terrain/${z}/${x}/${y}${devicePixelRatio > 1 ? "@2x" : ""}.png`,
-    "Stamen Toner": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/toner/${z}/${x}/${y}${devicePixelRatio > 1 ? "@2x" : ""}.png`,
-    "Stamen Toner (hybrid)": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/toner-hybrid/${z}/${x}/${y}${devicePixelRatio > 1 ? "@2x" : ""}.png`,
-    "Stamen Toner (lite)": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/toner-lite/${z}/${x}/${y}${devicePixelRatio > 1 ? "@2x" : ""}.png`,
+    "CartoDB Voyager": (x, y, z) => `https://${"abc"[Math.abs(x + y) % 3]}.basemaps.cartocdn.com/rastertiles/voyager/${z}/${x}/${y}@2x.png`,
+    "Stamen Terrain": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/terrain/${z}/${x}/${y}@2x.png`,
+    "Stamen Toner": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/toner/${z}/${x}/${y}@2x.png`,
+    "Stamen Toner (hybrid)": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/toner-hybrid/${z}/${x}/${y}@2x.png`,
+    "Stamen Toner (lite)": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/toner-lite/${z}/${x}/${y}@2x.png`,
     "Stamen Watercolor": (x, y, z) => `https://stamen-tiles-${"abc"[Math.abs(x + y) % 3]}.a.ssl.fastly.net/watercolor/${z}/${x}/${y}.png`,
     "OSM Mapnik": (x, y, z) => `https://${"abc"[Math.abs(x + y) % 3]}.tile.osm.org/${z}/${x}/${y}.png`,
     "Wikimedia Maps": (x, y, z) => `https://maps.wikimedia.org/osm-intl/${z}/${x}/${y}.png`
